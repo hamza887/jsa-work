6256533a73869794aa63e65fd56b95eaa70bde6d
hamza887 <hamzabutt887@hotmail.com>
2021-08-02 16:45:47 -0700

    working on js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
- console.log('world')
+ console.log('world');
+
+ console.log("hello this is a nice\n world");
+ console.log('fhdlskfjd');
+ console.log("dlkfj'dfjk'dfsad ");
+ console.log('dkjfhfd"dhfdkl"jd')
+
+ let hamza='hamza';
+ let index= 4
+ console.log(hamza.charAt(3));
+
+ console.log(typeof ('jonnu'+ 3))
+
+ const letters =[
+     ['a','b','c'],
+     ['d','e','f'],
+     ['g','h','i']
+ ]
+
+ console.log(letters[2][2]);
+
+ let sei={
+     students: ['hamza','hdlfj'],
+     curriclum:{
+         hard:true,
+         valuable: 'absoluty',
+     }
+ }
+
+ console.log(sei.curriclum.valuable);
