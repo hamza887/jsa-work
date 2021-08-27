b07c98d475f0a054c1af86e835d66d55f632e41d
hamza887 <hamzabutt887@hotmail.com>
2021-08-27 06:23:29 -0700

    woring constructors

--- netninja.js
+++ netninja.js
@@ -3,13 +3,24 @@
 class user {
     constructor(name,email){
         this.name=name;
-        this.email=email
+        this.email=email;
+        this.score=0;
     }
     adduser(){
         console.log(this.name ,'user has been added');
+        return this;
+    }
+    updateScore(){
+        console.log(this.score++);
+        return this;
     }
 }
 
-const hamza= new user('hamza','hamza@gmail.com')
+const hamza= new user('hamza','hamza@gmail.com');
+const john= new user('john','hamza@gmail.com')
 
 console.log(hamza);
+
+hamza.adduser();
+john.adduser();
+hamza.updateScore().updateScore().updateScore().adduser()
