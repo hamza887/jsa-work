3225cafc7a7acde0701a4c7f35cfede77f988cef
hamza887 <hamzabutt887@hotmail.com>
2021-08-27 05:22:22 -0700

    workin on classes

--- netninja.js
+++ netninja.js
@@ -1,1 +1,15 @@
-console.log('hello ninjas')
+
+
+class user {
+    constructor(name,email){
+        this.name=name;
+        this.email=email
+    }
+    adduser(){
+        console.log(this.name ,'user has been added');
+    }
+}
+
+const hamza= new user('hamza','hamza@gmail.com')
+
+console.log(hamza);
