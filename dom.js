3a59e05951d505b520cf86e3b74e6a1d3162b2cd
hamza887 <hamzabutt887@hotmail.com>
2021-08-29 05:35:25 -0700

    hello dom

--- dom.js
+++ dom.js
@@ -1,3 +1,13 @@
 
-const yourAnswer=prompt('Do you like apples?', 'Yes/No');
-(yourAnswer==='Yes')?alert("excellent"):'what are you saying'
+// const yourAnswer=prompt('Do you like apples?', 'Yes/No');
+// (yourAnswer==='Yes')?alert("excellent"):alert('what are you saying')
+
+const cool= document.getElementById('cool');
+const google=document.getElementById('google');
+console.log(cool);
+cool.style.textAlign='center';
+cool.innerHTML='hello how are you';
+cool.style.fontSize='55px';
+cool.style.color='red';
+
+console.log( google.setAttribute('href','https://www.google.com'));
