5ce9784caa6c06df7c31fd7680c9eeca784ba4bb
hamza887 <hamzabutt887@hotmail.com>
2021-08-30 10:17:53 -0700

    algos

---
+++ algos.js
@@ -0,0 +1,10 @@
+console.log('hello world');
+
+const charCount=(words)=>{
+   if(typeof words==='string'){
+   return words.length 
+   }else{
+       return 'hello this is wring'
+   }
+}
+console.log(charCount(123));
